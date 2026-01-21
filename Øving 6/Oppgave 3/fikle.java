675fafeab62f20d6d590933465c61d183af3dc09
Daniel Skjørtorp <daniel.skjortorp@gmail.com>
2026-01-21 12:22:08 +0100

    Delete Øving 6 directory

--- Øving 6/Oppgave 3/fikle.java
+++
@@ -1,9 +0,0 @@
-public class fikle {
-    public static void main(String[] args) {
-        matrise obj = new matrise();
-
-        obj.pluss();
-        obj.tran();
-        obj.multi();
-    }
-    }
