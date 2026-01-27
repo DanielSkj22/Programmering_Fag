5bf2754e4195868a6af6f1145866643bde9c1c05
dan <daniel.skjortorp@gmail.com>
2026-01-27 21:58:23 +0100

    Legger til noen metoder til replace text klassene

--- Programmering_2/Arbeidskrav/A3/src/main/java/org/idatt2003/ReplaceFirstTextCommand.java
+++ Programmering_2/Arbeidskrav/A3/src/main/java/org/idatt2003/ReplaceFirstTextCommand.java
@@ -1,4 +1,8 @@
 package org.idatt2003;
 
 public class ReplaceFirstTextCommand {
+    public ReplaceFirstTextCommand(String target, String replacement) {
+    }
+
+
 }
--- Programmering_2/Arbeidskrav/A3/src/main/java/org/idatt2003/ReplaceTextCommand.java
+++ Programmering_2/Arbeidskrav/A3/src/main/java/org/idatt2003/ReplaceTextCommand.java
@@ -1,4 +1,16 @@
 package org.idatt2003;
 
 public class ReplaceTextCommand {
+    protected String target;
+    protected String replacement;
+    public ReplaceTextCommand(String target, String replacement){
+        this.target = target;
+        this.replacement = replacement;
+    }
+    public String getTarget(){
+        return target;
+    }
+    public String getReplacement(){
+        return replacement;
+    }
 }
