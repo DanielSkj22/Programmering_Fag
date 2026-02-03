630bd6fbd71af4e1238abfce4a2aeb982de796cc
dan <daniel.skjortorp@gmail.com>
2026-02-03 10:32:55 +0100

    Replace metoder satt opp

---
+++ Programmering_2/Arbeidskrav/A3/src/main/java/org/example/idatt2003/ReplaceFirstTextCommand.java
@@ -0,0 +1,9 @@
+package org.example.idatt2003;
+
+public class ReplaceFirstTextCommand {
+    public ReplaceFirstTextCommand(String target, String replacement){
+    }
+    public String execute(String text){
+        return text;
+    }
+}
--- Programmering_2/Arbeidskrav/A3/src/main/java/org/example/idatt2003/ReplaceTextCommand.java
+++ Programmering_2/Arbeidskrav/A3/src/main/java/org/example/idatt2003/ReplaceTextCommand.java
@@ -1,4 +1,14 @@
 package org.example.idatt2003;
 
-public class ReplaceTextCommand {
+public class ReplaceTextCommand extends ReplaceFirstTextCommand{
+    protected String target;
+    protected String replacement;
+    public ReplaceTextCommand(String target, String replacement){
+        super(target, replacement);
+        this.target = target;
+        this.replacement = replacement;
+    }
+    public String execute(String text){
+        return text;
+    }
 }
